93f9475a645f3f604c7e8fa0bc1911a85a5a25f0
Chris <chris.nguyen0017@gmail.com>
2019-11-27 13:34:24 -0500

    Minor changes and new comments

--- src/main/java/featureTODO.java
+++ src/main/java/featureTODO.java
@@ -1,2 +1,22 @@
-public class featureTODO {
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class featureTODO extends ListenerAdapter {
+
+    /* !todoadd !todoview !todoremove [id] */
+    /* todoList(id, entry, timeAdded, isCompleted)*/
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] messageSent = event.getMessage().getContentRaw().split(" ");
+
+        if (messageSent[0].equalsIgnoreCase("!todoadd")) {
+
+        }
+        else if (messageSent[0].equalsIgnoreCase("!todoview")) {
+
+        }
+        else if (messageSent[0].equalsIgnoreCase("todoremove")) {
+
+        }
+    }
 }
