afc3b5cd9c9a8b41995545939f832def9bd630d3
Chris <chris.nguyen0017@gmail.com>
2019-12-22 00:41:23 -0500

    todo feature

--- src/main/java/featureHelp.java
+++ src/main/java/featureHelp.java
@@ -11,12 +11,39 @@ public class featureHelp extends ListenerAdapter{
         }
 
         if (event.getMessage().getContentRaw().equalsIgnoreCase("!rosiecommands")) {
-            event.getChannel().sendMessage("Hello and thank you for using me!\n" + "\n" + "I am a personal assistant Discord Bot that aims to improve quality of life and reduce number of trivialities one faces in their everyday life.\n" +
-                    "\n" + "**FEATURES LIST**\n" + "\n" + "**HELP**\n" + "> Ping! - tests to see if I am up and running. I should respond with, \"Pong!\" if so!\n" + "> !rosiecommands - if you want to know about me and all the things I can do, type this command.\n" +
-                    "\n" + "**ARITHMETIC**\n" + "> !calculate [number1] [+, -, *, /, %] [number2] - can compute basic math.\n" + "\n" + "**RNG RESPONSE**\n" + "> !coin - responds with HEADS or TAILS.\n" + "> !dice - responds with a random dice roll from 0-6.\n" +
-                    "> !card - responds with a random card.\n" + "> !numberbetween [number1] [number2] - responds with a random number between these two values. Whole numbers only. [number1] must always be a smaller number than [number2].\n" +
-                    "\n" + "**REMINDERS**\n" + "> !remindme [number] [unit] [reminder ...] - sets reminders for you and will send you a direct message accordingly. Units can be s, seconds, m, minutes, h, or hours.\n" + "\n" +
-                    "**REDDIT**\n" + "> !reddit [name of subreddit] - responds with the top 5 current hottest posts of a subreddit.").queue();
+            event.getChannel().sendMessage("Hello and thank you for using me!\n" +
+                    "\n" +
+                    "I am a personal assistant Discord Bot that aims to improve quality of life and reduce number of trivialities one faces in their everyday life.\n" +
+                    "\n" +
+                    "**FEATURES LIST**\n" +
+                    "\n" +
+                    "**HELP**\n" +
+                    "> Ping! - tests to see if I am up and running. I should respond with, \\\"Pong!\\\" if so!\n" +
+                    "> !rosiecommands - if you want to know about me and all the things I can do, type this command.\n" +
+                    "\n" +
+                    "**ARITHMETIC**\n" +
+                    "> !calculate [number1] [+, -, *, /, %] [number2] - can compute basic math.\n" +
+                    "\n" +
+                    "**RNG RESPONSE**\n" +
+                    "> !coin - responds with HEADS or TAILS.\n" +
+                    "> !dice - responds with a random dice roll from 0-6.\n" +
+                    "> !card - responds with a random card.\n" +
+                    "> !numberbetween [number1] [number2] - responds with a random number between these two values; whole numbers only. [number1] must always be a smaller number than [number2].\n" +
+                    "\n" +
+                    "**REMINDERS**\n" +
+                    "> !remindme [number] [unit] [query] - sets reminders for you and will send you a direct message accordingly. Units can be s, seconds, m, minutes, h, or hours.\n" +
+                    "\n" +
+                    "**REDDIT**\n" +
+                    "> !reddit [name of subreddit] - responds with the top 5 current hottest posts of a subreddit.\n" +
+                    "\n" +
+                    "**GOOGLE SEARCH**\n" +
+                    "> !googlesearch [query] - responds with the top 3 Google searches for that query.\n" +
+                    "\n" +
+                    "**TO DO LIST**\n" +
+                    "> !todoadd [query] - add an entry to your list\n" +
+                    "> !todoview [query] - view your current list of items\n" +
+                    "> !todocomplete [query] - check an item on your list as complete (does not remove it)\n" +
+                    "> !todoremove [query] - remove an item on your list").queue();
         }
     }
 }
