51b8e1b7f2989cde8ebc16b03b0a6c19dc5d5415
Christopher Nguyen <chrisngyn99@gmail.com>
2020-01-17 01:24:31 -0500

    im sleepy

---
+++ src/main/java/chrisngyn/github/rosie/CommandsHandler.java
@@ -0,0 +1,10 @@
+package chrisngyn.github.rosie;
+
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class CommandsHandler extends ListenerAdapter {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        
+    }
+}
