f93fe3b9c3a5a7b9873ec76c301c433c168ca792
Chris <chrisngyn99@gmail.com>
2020-01-16 23:29:03 -0500

    test2

---
+++ src/main/java/chrisngyn/github/rosie/commands/Ping.java
@@ -0,0 +1,14 @@
+package chrisngyn.github.rosie.commands;
+
+import chrisngyn.github.rosie.Commands;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Ping extends Commands {
+    public Ping() {
+        super("ping");
+    }
+
+    public void execute(MessageReceivedEvent event, String[] args) {
+        event.getChannel().sendMessage("Pong!");
+    }
+}
