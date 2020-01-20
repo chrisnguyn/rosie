5475560aec11ca062d237c1c1e77b943fadd49b7
Christopher Nguyen <chrisngyn99@gmail.com>
2020-01-20 14:30:06 -0500

    beepo

--- src/main/java/chrisngyn/github/rosie/Command.java
+++ src/main/java/chrisngyn/github/rosie/Command.java
@@ -1,12 +1,19 @@
 package chrisngyn.github.rosie;
 
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public abstract class Command {
 
     private final String name;
 
-    public Command(String name) { this.name = name; }
-    public abstract void execute(MessageReceivedEvent event, String[] args);
-    public String getName() { return name; }
+    public Command(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void execute(GuildMessageReceivedEvent event, String[] args);
+
 }
--- src/main/java/chrisngyn/github/rosie/commands/Ping.java
+++ src/main/java/chrisngyn/github/rosie/commands/Ping.java
@@ -1,7 +1,7 @@
 package chrisngyn.github.rosie.commands;
 
 import chrisngyn.github.rosie.Command;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class Ping extends Command {
 
@@ -9,7 +9,7 @@ public class Ping extends Command {
         super("Ping");
     }
 
-    public void execute(MessageReceivedEvent event, String[] args) {
+    public void execute(GuildMessageReceivedEvent event, String[] args) {
         event.getChannel().sendMessage("Pong!").queue();
     }
 }
