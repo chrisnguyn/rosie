354b2385e24c5757270149fec0cf3a3a21e2524c
Christopher Nguyen <chrisngyn99@gmail.com>
2020-01-20 22:20:27 -0500

    beepo

--- src/main/java/chrisngyn/github/rosie/CommandsHandler.java
+++ src/main/java/chrisngyn/github/rosie/CommandsHandler.java
@@ -9,8 +9,7 @@ import java.util.Map;
 public class CommandsHandler extends ListenerAdapter {
 
     private final Map<String, Command> commands = new HashMap<>();
-    private final static String prefix = "!";
-    String name;
+    private final static String PREFIX = "!";
 
     private CommandsHandler() {
         Ping ping = new Ping();
@@ -19,9 +18,22 @@ public class CommandsHandler extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        Command command = commands.get(name);
+
+        String message = event.getMessage().getContentRaw();
+
+        if (event.getAuthor().isBot() || !message.startsWith(PREFIX)) {
+            return; // no need to continue at this point and create arrays and waste memory.
+        }
+
+        // todo: check that args[0].length is greater than 1 before trying to substring otherwise exceptions raised
+
+        String[] arguments = message.split(" "); // if you get to this point, you know someone is trying to execute a cmd.
+        String name = arguments[0].substring(PREFIX.length()).toLowerCase(); // cut out the designated prefix. !add becomes add
+
+        Command command = commands.get(name); // nab the name of the command if exists, otherwise it'll return null
+
         if(command != null) {
-            command.execute(event, null);
+            command.execute(event, arguments);
         }
     }
 }
