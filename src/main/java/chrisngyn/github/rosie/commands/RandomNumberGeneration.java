bd21d057fdb9b3d98db59392af692e3debe73686
Chris N <chrisngyn99@gmail.com>
2020-01-24 00:33:23 -0500

    Updated RNG
    12:33am and it came to me GG

--- src/main/java/chrisngyn/github/rosie/commands/RandomNumberGeneration.java
+++ src/main/java/chrisngyn/github/rosie/commands/RandomNumberGeneration.java
@@ -1,4 +1,22 @@
 package chrisngyn.github.rosie.commands;
 
 public class RandomNumberGeneration {
+  
+  /*
+      CURRENTLY DO NOT HAVE ACCESS TO MY COMPUTER BUT IT IS 12:31AM AND I NEED TO WRITE THIS BEFORE I FORGET LOL
+      
+      !choose [arg1] | [arg2]
+      
+      public void execute(Event event, String[] args) {
+      
+          event.getChannel().getContent() gives "!choose [arg1] | [arg2]"
+          String t = event.getChannel().getContent().substring(args[0].length) gives "[arg1] | [arg2]"
+          String[] x = t.split(" | "); gives [arg1, arg2]
+          
+          make a random obj
+          use it to decide one or the other
+          gg
+      }
+  */
+  
 }
