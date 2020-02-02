3de8bdc10f1fa3b0bbfc9315373a17873e1d7526
Christopher Nguyen <chrisngyn99@gmail.com>
2020-02-02 03:37:55 -0500

    music module incomplete, base class

---
+++ src/main/java/chrisngyn/github/rosie/commands/Music.java
@@ -0,0 +1,20 @@
+package chrisngyn.github.rosie.commands;
+
+import chrisngyn.github.rosie.Command;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+public class Music extends Command {
+
+    String documentation = "**NOT YET IMPLEMENTED.**";
+
+    // https://github.com/DV8FromTheWorld/JDA/wiki/4)-Making-a-Music-Bot
+
+    public Music() {
+        super("");
+    }
+
+    public void execute(GuildMessageReceivedEvent event, String[] args) {
+
+    }
+
+}
