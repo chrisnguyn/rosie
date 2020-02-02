f39f103ed75f78d3c7998378c24f4d3a3a73f86b
Christopher Nguyen <chrisngyn99@gmail.com>
2020-02-01 21:18:22 -0500

    beep

--- src/main/java/chrisngyn/github/rosie/commands/GoogleSearch.java
+++ src/main/java/chrisngyn/github/rosie/commands/GoogleSearch.java
@@ -3,17 +3,33 @@ package chrisngyn.github.rosie.commands;
 import chrisngyn.github.rosie.Command;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class GoogleSearch extends Command {
 
-    // How to call: !googlesearch [query]
-    // https://www.googleapis.com/customsearch/v1?q=[QUERY_HERE]&cx=[SEARCH_ENGINE_ID_HERE]&num=3&key=[API_KEY_HERE]
-
-    public GoogleSearch() {
+    public GoogleSearch() throws Exception {
         super("googlesearch");
+        String file = "googlecredentials.txt";
+        FileReader reader = new FileReader(file);
+        BufferedReader buffer = new BufferedReader(reader);
+        String searchEngineID = buffer.readLine();
+        String APIKey = buffer.readLine();
+        buffer.close();
+        reader.close();
+        String credentials[] = {searchEngineID, APIKey};
     }
 
-    public void execute(GuildMessageReceivedEvent event, String[] args) {
+    public void execute(GuildMessageReceivedEvent event, String[] args) { // !googlesearch tesla
+        String query = "";
+        for (int i = 3; i < args.length; i++) { query += args[i] + " "; }
 
+        try {
+            MyGETRequest(event, query);
+        }
+        catch (Exception e) {
+            System.err.println(e);
+        }
     }
 
 }
