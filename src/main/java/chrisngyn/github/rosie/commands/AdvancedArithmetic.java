c3c50c959bc8a3d61feecdbb4a9d063f5da4feb9
Christopher Nguyen <chrisngyn99@gmail.com>
2020-04-07 18:44:52 -0400

    Refactored a few lines

--- src/main/java/chrisngyn/github/rosie/commands/AdvancedArithmetic.java
+++ src/main/java/chrisngyn/github/rosie/commands/AdvancedArithmetic.java
@@ -4,9 +4,6 @@ import chrisngyn.github.rosie.Command;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class AdvancedArithmetic extends Command {
-
-    // I hope you're happy Bonnie smh. Need me to make these features because you're too lazy to do math on your own. >:-\
-
     protected String documentation = "**!moremath quadratic** [number1] [number2] [number3] - quadratic formula, enter values to get the roots of an equation. \n" +
             "> **!moremath pythagorean** [number1] [number2] - pythagorean theorem, enter two side lengths to get the hypotenuse. \n" +
             "> **!moremath** [**sin**, **cos**, **tan**, **c2f**, **f2c**] [number] - sin, cos, tan, celsius to fahrenheit, and fahrenheit to celsius conversions.";
@@ -16,56 +13,60 @@ public class AdvancedArithmetic extends Command {
     }
 
     @Override
-    public void execute(GuildMessageReceivedEvent event, String[] args) { // !moremath quadratic, pythagorean, sin, cos, tan, c2f, f2c
+    public void execute(GuildMessageReceivedEvent event, String[] args) { // error checking to ensure they're using command properly?
+        String ret = "";
         switch (args[1]) {
-            case "quadratic": // !moremath quadratic 1 2 3
+            case "quadratic":
                 double a = Double.parseDouble(args[2]);
                 double b = Double.parseDouble(args[3]);
                 double c = Double.parseDouble(args[4]);
                 double discriminant = (b * b) - (4 * a * c);
-
                 if (discriminant > 0) {
                     double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
                     double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-                    event.getChannel().sendMessage("As your discriminant is positive, you have two roots. Your roots are: **" + root1 + "** and **" + root2 + "**").queue();
-                }
-                else if (discriminant == 0) {
+                    ret = "As your discriminant is positive, you have two roots. Your roots are: **" + root1 + "** and **" + root2 + "**";
+                } else if (discriminant == 0) {
                     double root = (-b / (2 * a));
-                    event.getChannel().sendMessage("As your discriminant is zero, you have one root. Your root is: **" + root + "**").queue();
-                }
-                else {
-                    event.getChannel().sendMessage("As your discriminant is negative, you have zero real roots.").queue();
+                    ret = "As your discriminant is zero, you have one root. Your root is: **" + root + "**";
+                } else {
+                    ret = "As your discriminant is negative, you have zero real roots.";
                 }
                 break;
 
-            case "pythagorean": // !moremath pythagorean 3 4 - get "5"
+            case "pythagorean":
                 double num1 = Double.parseDouble(args[2]);
                 double num2 = Double.parseDouble(args[3]);
                 double hypotenuse = (Math.sqrt((Math.pow(num1, 2) + Math.pow(num2, 2))));
-                event.getChannel().sendMessage("The hypontenuse of the two side length you have provided is: **" + hypotenuse + "**").queue();
+                ret = "The hypontenuse of the two side length you have provided is: **" + hypotenuse + "**";
                 break;
 
-            case "sin": // !moremath sin 70
-                event.getChannel().sendMessage("Result: **" + Math.sin(Integer.parseInt(args[2])) + "**").queue();
+            case "sin":
+                ret = "Result: **" + Math.sin(Integer.parseInt(args[2])) + "**";
                 break;
 
             case "cos":
-                event.getChannel().sendMessage("Result: **" + Math.cos(Integer.parseInt(args[2])) + "**").queue();
+                ret = "Result: **" + Math.cos(Integer.parseInt(args[2])) + "**";
                 break;
 
             case "tan":
-                event.getChannel().sendMessage("Result: **" + Math.tan(Integer.parseInt(args[2])) + "**").queue();
+                ret = "Result: **" + Math.tan(Integer.parseInt(args[2])) + "**";
                 break;
 
-            case "c2f": // !moremath c2f 70
+            case "c2f":
                 double c2f = ((Double.parseDouble(args[2]) * 9) / 5) + 32;
-                event.getChannel().sendMessage(args[2] + " degrees Celsius to Fahrenheit is: **" + c2f + "**").queue();
+                ret = args[2] + " degrees Celsius to Fahrenheit is: **" + c2f + "**";
                 break;
 
             case "f2c":
                 double f2c = ((Double.parseDouble(args[2]) - 32) * 5) / 9;
-                event.getChannel().sendMessage(args[2] + " degrees Fahrenheit to Celsius is: **" + f2c + "**").queue();
+                ret = args[2] + " degrees Fahrenheit to Celsius is: **" + f2c + "**";
                 break;
+
+            default:
+                event.getChannel().sendMessage("Unknown operation. Please consult !help for documentation.").queue();
+                return;
         }
+
+        event.getChannel().sendMessage(ret);
     }
 }
--- src/main/java/chrisngyn/github/rosie/commands/Arithmetic.java
+++ src/main/java/chrisngyn/github/rosie/commands/Arithmetic.java
@@ -4,8 +4,7 @@ import chrisngyn.github.rosie.Command;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class Arithmetic extends Command {
-
-    // \* is not an escape character in Java, so we do \\* to get \* to get * ...get it?
+    // "\*" is not an escape character in Java, so we do \\* to get \* to get * ...get it?
     protected String documentation = "**!calculate** [number1] [**+**, **-**, **\\***, **/**, **%**, **^**] [number2] - basic computation.";
 
     public Arithmetic() {
@@ -13,35 +12,45 @@ public class Arithmetic extends Command {
     }
 
     @Override
-    public void execute(GuildMessageReceivedEvent event, String[] args) { // !calculate 10 + 10
+    public void execute(GuildMessageReceivedEvent event, String[] args) {
         if (args.length != 4) {
             event.getChannel().sendMessage("Improper use of command. Please consult !help for documentation.").queue();
         } else {
             Double num1 = Double.parseDouble(args[1]);
             Double num2 = Double.parseDouble(args[3]);
+            Double ret = 0.0;
 
             switch (args[2]) {
                 case "+":
-                    event.getChannel().sendMessage("Result: " + (num1 + num2)).queue();
+                    ret = num1 + num2;
                     break;
+
                 case "-":
-                    event.getChannel().sendMessage("Result: " + (num1 - num2)).queue();
+                    ret = num1 - num2;
                     break;
+
                 case "*":
-                    event.getChannel().sendMessage("Result: " + (num1 * num2)).queue();
+                    ret = num1 * num2;
                     break;
+
                 case "/":
-                    event.getChannel().sendMessage("Result: " + (num1 / num2)).queue();
+                    ret = num1 / num2;
                     break;
+
                 case "%":
-                    event.getChannel().sendMessage("Result: " + (num1 % num2)).queue();
+                    ret = num1 % num2;
                     break;
+
                 case "^":
-                    event.getChannel().sendMessage("Result: " + Math.pow(num1, num2)).queue();
+                    ret = Math.pow(num1, num2);
                     break;
+
                 default:
                     event.getChannel().sendMessage("Unknown operation. Please consult !help for documentation.").queue();
+                    return;
             }
+
+            event.getChannel().sendMessage("Result: " + "**" + ret + "**").queue();
         }
     }
 }
