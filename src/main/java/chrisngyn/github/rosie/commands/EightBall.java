a78660343dcdfdf41b85a4a9a0fab76611b90937
Christopher Nguyen <chrisngyn99@gmail.com>
2020-04-07 18:48:13 -0400

    Updated RANDOM RESPONSE commands, added comments, removed redundant ones

--- src/main/java/chrisngyn/github/rosie/commands/EightBall.java
+++ src/main/java/chrisngyn/github/rosie/commands/EightBall.java
@@ -4,26 +4,22 @@ import chrisngyn.github.rosie.Command;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import java.util.Random;
 
-public class EightBall extends Command {
-
-    // https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers
-
+public class EightBall extends Command { // https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers
     protected String documentation = "**!8ball** [anything] - get a random Magic 8-Ball response!";
-
-    Random random = new Random();
     private final String[] responses = { "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.",
             "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
             "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
             "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful." };
+    Random random = new Random();
 
     public EightBall() {
         super("8Ball");
     }
 
-    public void execute(GuildMessageReceivedEvent event, String[] args) { // !8ball ...
-        System.out.println(responses.length);
-        int index = random.nextInt(responses.length); // length 20, range is 0 - 19
+    @Override
+    public void execute(GuildMessageReceivedEvent event, String[] args) {
+        System.out.println(responses.length); // testing purposes
+        int index = random.nextInt(responses.length); // length 20, therefore the range is from 0 - 19
         event.getChannel().sendMessage("**" + responses[index] + "**").queue();
     }
-
 }
--- src/main/java/chrisngyn/github/rosie/commands/RandomNumberGeneration.java
+++ src/main/java/chrisngyn/github/rosie/commands/RandomNumberGeneration.java
@@ -6,73 +6,65 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class RandomNumberGeneration extends Command {
-
     protected String documentation = "**!random coin** - flip a coin.\n" +
             "> **!random dice** - roll a die.\n" +
             "> **!random card** - draw a card.\n" +
             "> **!random choose** option1 | option2 | option3 | ... - choose an option.\n" +
             "> **!random between** [number1] [number2] - get a number in that range (inclusive).";
-
-    Random random = new Random(); // doesn't need to be inside of execute, why create new object every call?
-
     private final String[] value = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}; // 0-13
     private final String[] suit = {"Clubs", "Spades", "Hearts", "Diamonds"}; // 0-3
+    Random random = new Random(); // doesn't need to be inside of execute; why create new object every call?
 
     public RandomNumberGeneration() {
         super("random");
     }
 
-    // compiler angry if we try to Override a method that DOESN'T exist in superclass
-    // if we have "public void myMethod()" with an Override tag, it'll get angry because there isn't one in super called that
-    // no @Override tag on a method that exists in super - fine, whatever, but bad practise
-    // no @Override tag on a method that doesn't exist in super - it'll allow it even though you expect it to be a super method
-    // maybe you typed it wrong. for example, imagine you called it GuildMessageRecIEvedEvent (misspelled), it'll allow it if no tag
-
     @Override
     public void execute(GuildMessageReceivedEvent event, String[] args) {
-
-        // NOTE: !random card works, but !random CARD will not. this IS case sensitive
+        String ret = "";
         switch (args[1].toLowerCase()) {
             case "coin":
                 int coin = random.nextInt(2); // 0 or 1.
                 String headsOrTails = (coin == 0 ? "HEADS" : "TAILS");
-                event.getChannel().sendMessage("Result of your coin flip: **" + headsOrTails + "**").queue();
+                ret = "Result of your coin flip: **" + headsOrTails + "**";
                 break;
 
             case "dice":
-                int dice = random.nextInt(6) + 1; // 0-5, +1, 1-6 instead of 0-6 by making bound 7
-                event.getChannel().sendMessage("Result of your dice roll: **" + dice + "**").queue();
+                int dice = random.nextInt(6) + 1; // 0-5, +1 = 1-6 instead of having 0-6 by making bound 7
+                ret = "Result of your dice roll: **" + dice + "**";
                 break;
 
-            case "card": // 2 3 4 5 6 7 8 9 10 J K Q A, Clubs Spades Diamonds Hearts
-                int v = random.nextInt(14); // 0 - 13
-                int s = random.nextInt(4); // 0 - 3 [val] of [suit]
-                String ret = "Result: **" + value[v] + " of " + suit[s] + "**";
-                event.getChannel().sendMessage(ret).queue();
+            case "card":
+                int val = random.nextInt(14); // 0 - 13
+                int suit = random.nextInt(4); // 0 - 3 [val] of [suit]
+                ret = "Result: **" + this.value[val] + " of " + this.suit[suit] + "**"; // this.suit to denote our global variable and not the integer we made
                 break;
 
-            case "choose": // !random choose [arg1] | [arg2]
+            case "choose":
                 String content = event.getMessage().getContentRaw().substring((args[0] + args[1]).length() + 2); // gives "[arg1] | [args2]"
                 String[] contentSpliced = content.split(" \\| "); // gives an array of [arg1, arg2]; currently count 2
                 int choose = random.nextInt(contentSpliced.length); // length = 2, bound = 2, range 0-1.
                 System.out.println(Arrays.toString(contentSpliced));
-                event.getChannel().sendMessage("I choose: **" + contentSpliced[choose] + "**").queue();
+                ret = "I choose: **" + contentSpliced[choose] + "**";
                 break;
 
-            case "between": // !random between 10 20
-                if (Integer.parseInt(args[2]) > Integer.parseInt(args[3])) {
+            case "between":
+                if (Integer.parseInt(args[2]) > Integer.parseInt(args[3]) || Integer.parseInt(args[2]) == Integer.parseInt(args[3])) {
                     event.getChannel().sendMessage("Your lower bound can't be less than or equal to your upper bound! :angry:").queue();
-                }
-                else {
+                    return;
+                } else {
                     int max = Integer.parseInt(args[3]);
                     int min = Integer.parseInt(args[2]);
                     int result = random.nextInt((max - min) + 1) + min; // algorithm to find numbers inclusive
-                    event.getChannel().sendMessage("Result: " + result).queue();
+                    ret = "Result: " + result;
                 }
                 break;
 
             default:
                 event.getChannel().sendMessage("Improper use of command. Please type !help for documentation.").queue();
+                return;
         }
+
+        event.getChannel().sendMessage(ret).queue();
     }
 }
