8fb9830ab493dcba209818a7acd31423b7966dcc
Christopher Nguyen <chrisngyn99@gmail.com>
2020-04-07 18:49:15 -0400

    Updated Reminder class

--- src/main/java/chrisngyn/github/rosie/commands/Reminder.java
+++ src/main/java/chrisngyn/github/rosie/commands/Reminder.java
@@ -7,16 +7,15 @@ import java.util.concurrent.TimeUnit;
 import static java.util.concurrent.TimeUnit.*;
 
 public class Reminder extends Command {
-
     protected String documentation = "**!remindme** [number] [s, seconds, m, minutes, h, hours] [reminder] - set a reminder; Rosie will send you a direct message reminding you when time is up.";
+    String reminder = "Hello! You wanted me to remind you of this: **";
 
     public Reminder() {
-        super("remindme"); // !remindme 20 minutes take the pizza out of the oven
+        super("remindme");
     }
 
-    public void execute(GuildMessageReceivedEvent event, String[] args) { // !remindme 20 minutes take pizza out of oven / any arbitrary-n length string
-
-        String reminder = "Hello! You wanted me to remind you of this: **";
+    @Override
+    public void execute(GuildMessageReceivedEvent event, String[] args) {
         for (int i = 3; i < args.length; i++) { reminder += args[i] + " "; }
 
         switch (args[2]) {
@@ -34,17 +33,17 @@ public class Reminder extends Command {
                 break;
             default:
                 event.getChannel().sendMessage("Unknown time argument. Please consult !help for documentation.").queue();
+                return;
         }
 
     }
 
-    public void remind(GuildMessageReceivedEvent event, User user, String reminder, long delay, TimeUnit unit) { // helper method. personally modified to include an extra param.
-        // IF NOT WORKING, IT'S MOST LIKELY BECAUSE THE PROJECT LANGUAGE YOU HAVE SET DOES NOT SUPPORT LAMBDA EXPRESSIONS.
+    public void remind(GuildMessageReceivedEvent event, User user, String reminder, long delay, TimeUnit unit) {
+        // IF NOT WORKING, IT'S MOST LIKELY BECAUSE THE PROJECT LANGUAGE YOU HAVE SET DOES NOT SUPPORT LAMBDA EXPRESSIONS!
         // Fix 1. File -> Settings -> Project bytecode version: 8, and delete all per-module bytecode versions
         // Fix 2. File -> Project Structure -> Language level 8
         // Fix 3. IntelliJ IDEA -> Preferences -> change project bytecode from 1.5 to 1.8
-        user.openPrivateChannel().queue((channel) -> channel.sendMessage(reminder + "**").queueAfter(delay, unit)); // ctrl + click for more info
+        user.openPrivateChannel().queue((channel) -> channel.sendMessage(reminder + "**").queueAfter(delay, unit));
         event.getChannel().sendMessage("Your reminder has been set!").queue();
     }
-
 }
