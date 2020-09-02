89822cefa3f9ecf17a53ebb84d8a8fe349dca5e6
Chris N <chrisngyn99@gmail.com>
2020-09-02 13:26:05 -0400

    [WIP] Abstracting todo list cmds into functions (#9)
    * todoAdd and todoView completed
    
    * todoComplete todoRemove done

--- src/main/java/chrisngyn/github/rosie/commands/ToDoList.java
+++ src/main/java/chrisngyn/github/rosie/commands/ToDoList.java
@@ -32,7 +32,7 @@ public class ToDoList extends Command {
             this.connection = createDBConnection();
         } catch (Exception e) {
             System.err.println(e + "\n Error trying to build ToDoList instance.");
-            this.error = "Error executing ToDo command. Please contact bot creator.";
+            this.error = "Error executing todo command. Please contact bot creator.";
         }
     }
 
@@ -53,87 +53,51 @@ public class ToDoList extends Command {
 
         switch (args[1]) {
             case "add":
-                String add_content = "";
-                for (int i = 2; i < args.length; i++) { add_content += args[i] + " "; }
-                String add_query = "INSERT INTO rosie.featuretodo(user_id, user_query, when_added, is_completed) values (?, ?, ?, ?)";
+                String addContent = "";
+                for (int i = 2; i < args.length; i++) { addContent += args[i] + " "; }
 
-                try {
-                    PreparedStatement pstmt = this.connection.prepareStatement(add_query);
-                    pstmt.setLong(1, userId);
-                    pstmt.setString(2, add_content);
-                    pstmt.setString(3, formatter.format(date));
-                    pstmt.setString(4, "No");
-                    pstmt.execute();
+                if (todoAdd(addContent, userId, date, formatter) == 0) {
                     event.getChannel().sendMessage("Your entry has been added to your to do list!").queue();
-                } catch (Exception e) {
-                    System.err.println("Error executing query for TODO_ADD.");
-                    System.err.println(e);
+                } else {
                     event.getChannel().sendMessage("Sorry, but something went wrong. Please alert the bot creator!").queue();
                 }
+
                 break;
 
             case "view":
-                String view_query = String.format("SELECT featuretodo.user_query, featuretodo.when_added, featuretodo.is_completed FROM rosie.featuretodo WHERE featuretodo.user_id = (%d)", userId);
+                String author = event.getAuthor().getName();
+                EmbedBuilder response = todoView(author, userId, date, formatter);
 
-                try {
-                    Statement stmt = connection.createStatement();
-                    ResultSet rs = stmt.executeQuery(view_query);
-                    String entries = "";
-                    String added = "";
-                    String completed = "";
-                    while (rs.next()) {
-                        entries += rs.getString("featuretodo.user_query") + "\n\n";
-                        added += rs.getString("featuretodo.when_added") + "\n\n";
-                        completed += rs.getString("featuretodo.is_completed") + "\n\n";
-                    }
-                    EmbedBuilder eb = new EmbedBuilder();
-                    eb.setTitle(event.getAuthor().getName() + "'s to do list:");
-                    eb.addField("Your Entries", entries, true);
-                    eb.addField("Added When", added, true);
-                    eb.addField("Is Completed", completed, true);
-                    // eb.setThumbnail("https://www.calltrackingmetrics.com/wp-content/uploads/2017/11/shopify_glyph.png"); LOL
-                    eb.setColor(9168790); // or Color.(anything), or www.shodor.org/stella2java/rgbint.html
-                    eb.setFooter("Request was made at: " + date, null); // date or formatter.format(date);
-                    event.getChannel().sendMessage(eb.build()).queue();
-                } catch (Exception e) {
-                    System.err.println("Error executing query for TODO_VIEW.");
+                if (response == null) {
                     event.getChannel().sendMessage("Sorry, but something went wrong. Please alert the bot creator!").queue();
+                } else {
+                    event.getChannel().sendMessage(response.build()).queue();
                 }
+
                 break;
 
-            case "complete":
-                String complete_content = "";
-                for (int i = 2; i < args.length; i++) { complete_content += args[i] + " "; }
-                String complete_query = "UPDATE rosie.featuretodo SET featuretodo.is_completed = \"Yes\" WHERE featuretodo.user_id = ? AND user_query = ?";
+            case "complete": // if you try to 'complete' something that doesn't exist it'll still give confirmation :(
+                String completeContent = "";
+                for (int i = 2; i < args.length; i++) { completeContent += args[i] + " "; }
 
-                try {
-                    PreparedStatement pstmt = connection.prepareStatement(complete_query);
-                    pstmt.setLong(1, userId);
-                    pstmt.setString(2, complete_content);
-                    pstmt.execute();
+                if (todoComplete(completeContent, userId) == 0) {
                     event.getChannel().sendMessage("Your entry has been updated in your to do list!").queue();
-                } catch (Exception e) {
-                    System.err.println("Error executing query for TODO_COMPLETE.");
+                } else {
                     event.getChannel().sendMessage("Sorry, but something went wrong. Please alert the bot creator!").queue();
                 }
+
                 break;
 
-            // if user tries to delete an entry that doesn't exist, it'll still give confirmation; need a way to check first if it exists.
-            case "remove":
-                String remove_content = "";
-                for (int i = 2; i < args.length; i++) { remove_content += args[i] + " "; }
-                String remove_query = "DELETE FROM rosie.featuretodo WHERE featuretodo.user_id = ? AND user_query = ?";
+            case "remove": // if you try to 'remove' something that doesn't exist it'll still give confirmation :(
+                String removeContent = "";
+                for (int i = 2; i < args.length; i++) { removeContent += args[i] + " "; }
 
-                try {
-                    PreparedStatement pstmt = connection.prepareStatement(remove_query);
-                    pstmt.setLong(1, userId);
-                    pstmt.setString(2, remove_content);
-                    pstmt.execute();
+                if (todoRemove(removeContent, userId) == 0) {
                     event.getChannel().sendMessage("Your entry has been deleted in your to do list!").queue();
-                } catch (Exception e) {
-                    System.err.println("Error executing query for TODO_REMOVE!");
+                } else {
                     event.getChannel().sendMessage("Sorry, but something went wrong. Please alert the bot creator!").queue();
                 }
+
                 break;
 
             default:
@@ -141,29 +105,90 @@ public class ToDoList extends Command {
         }
     }
 
+    private int todoAdd(String content, long userId, Date date, SimpleDateFormat formatter) { // return 0 if successful, -1 otherwise
+        String query = "INSERT INTO rosie.featuretodo(user_id, user_query, when_added, is_completed) values (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement pstmt = this.connection.prepareStatement(query);
+            pstmt.setLong(1, userId);
+            pstmt.setString(2, content);
+            pstmt.setString(3, formatter.format(date));
+            pstmt.setString(4, "No");
+            pstmt.execute();
+            return 0;
+        } catch (Exception e) {
+            System.err.println(e + "\n Error executing query for todoAdd.");
+            return -1;
+        }
+    }
+
+    private EmbedBuilder todoView(String author, long userId, Date date, SimpleDateFormat formatter) {
+        String query = String.format("SELECT featuretodo.user_query, featuretodo.when_added, featuretodo.is_completed FROM rosie.featuretodo WHERE featuretodo.user_id = (%d)", userId);
+
+        try {
+            Statement stmt = this.connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            String entries = "";
+            String added = "";
+            String completed = "";
+
+            while (rs.next()) {
+                entries += rs.getString("featuretodo.user_query") + "\n\n";
+                added += rs.getString("featuretodo.when_added") + "\n\n";
+                completed += rs.getString("featuretodo.is_completed") + "\n\n";
+            }
+
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle(author + "'s to do list:");
+            eb.addField("Your Entries", entries, true);
+            eb.addField("Added When", added, true);
+            eb.addField("Is Completed", completed, true);
+            eb.setThumbnail("https://www.calltrackingmetrics.com/wp-content/uploads/2017/11/shopify_glyph.png");
+            eb.setColor(9168790); // or Color.(anything), or www.shodor.org/stella2java/rgbint.html
+            eb.setFooter("Request was made at: " + date, null); // date or formatter.format(date);
+            return eb;
+        } catch (Exception e) {
+            System.err.println(e + "\n Error executing query for todoView.");
+            return null;
+        }
+    }
+
+    private int todoComplete(String content, long userId) {
+        String query = "UPDATE rosie.featuretodo SET featuretodo.is_completed = \"Yes\" WHERE featuretodo.user_id = ? AND user_query = ?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setLong(1, userId);
+            pstmt.setString(2, content);
+            pstmt.execute();
+            return 0;
+        } catch (Exception e) {
+            System.err.println(e + "\n Error executing query for todoComplete.");
+            return -1;
+        }
+    }
+
+    private int todoRemove(String content, long userId) {
+        String query = "DELETE FROM rosie.featuretodo WHERE featuretodo.user_id = ? AND user_query = ?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setLong(1, userId);
+            pstmt.setString(2, content);
+            pstmt.execute();
+            return 0;
+        } catch (Exception e) {
+            System.err.println(e + "\n Error executing query for todoRemove.");
+            return -1;
+        }
+    }
+
     private Connection createDBConnection(){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver is deprecated
             return DriverManager.getConnection(this.url, this.user, this.password);
         } catch (Exception e){
             System.err.println(e + "\n Unable to create DB connection!");
             return null;
         }
-    }
-
-    private void todoAdd() {
-
-    }
-
-    private void todoView() {
-
-    }
-
-    private void todoComplete() {
-
-    }
-
-    private void todoRemove() {
-
     }
 }
